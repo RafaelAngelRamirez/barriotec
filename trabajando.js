b90d6caa5f516906273d2411142db318a17edb90
Rafael Ramirez <rafa.yael@gmail.com>
2021-11-09 21:22:45 -0600

    fix: Error por que no existe el elemento

--- trabajando.js
+++ trabajando.js
@@ -1,15 +1,17 @@
-//
-// This file is meant to regroup your javascript code. You can either copy/past
-// any code that should be executed on each page loading or write your own
-// taking advantage of the Odoo framework to create new behaviors or modify
-// existing ones. For example, doing this will greet any visitor with a 'Hello,
-// world !' message in a popup:
-//
-
 odoo.define("website.user_custom_code", function (require) {
   "use strict"
 
   require("web.dom_ready")
+
+  const encabezados = [
+    ["Nombre", "name"],
+    ["Categoría", "public_categ_ids"],
+    ["Num de cuartos", "booking_rom_num"],
+    ["Piso", "booking_floor"],
+    ["Área del departamento", "booking_area"],
+    ["Área del balcon", "booking_lookout_area"],
+    ["Estado", "is_booking_type"],
+  ]
 
   function docReady(fn) {
     // see if DOM is already available
@@ -24,6 +26,12 @@ odoo.define("website.user_custom_code", function (require) {
     }
   }
 
+  /**
+   *Genera los encabezados y comprueba si debemos continuar
+   *
+   * @param {*} encabezados
+   * @returns false si no se ha detectado el id indicado
+   */
   function generarEncabezado(encabezados) {
     let headerTemplate = document.getElementById(
       "depa_disponibilidad_template_head"
@@ -31,7 +39,7 @@ odoo.define("website.user_custom_code", function (require) {
 
     //Agregamos uno al principio y al final
     // para estilizar
-
+    if (!headerTemplate) return falaw
     let trHead = headerTemplate.parentElement
 
     encabezados.forEach(x => {
@@ -42,6 +50,7 @@ odoo.define("website.user_custom_code", function (require) {
     })
 
     headerTemplate.remove()
+    return true
   }
 
   function generarDatos(datos) {
@@ -69,26 +78,15 @@ odoo.define("website.user_custom_code", function (require) {
   }
 
   function cargarDatos(datos) {
-    let encabezados = [
-      ["Nombre", "name"],
-      ["Categoría", "public_categ_ids"],
-      ["Num de cuartos", "booking_rom_num"],
-      ["Piso", "booking_floor"],
-      ["Área del departamento", "booking_area"],
-      ["Área del balcon", "booking_lookout_area"],
-      ["Estado", "is_booking_type"],
-    ]
-
-    generarEncabezado(encabezados.map(x => x[0]))
-
+    let continuar = generarEncabezado(encabezados.map(x => x[0]))
+    if (!continuar) return
     function getProductBySKU() {
       const model = "product.template"
       const method = "search_read"
       // Use an empty array to search for all the records
       const domain = []
       // Use an empty array to read all the fields of the records
-      //   const fields = encabezados.map(x => x[1])
-      const fields = ["name"]
+      const fields = encabezados.map(x => x[1])
       const options = {
         model,
         method,
@@ -98,17 +96,12 @@ odoo.define("website.user_custom_code", function (require) {
       rpc
         .query(options)
         .then(products => {
-          // alert(products)
-          //   console.log({ products })
           let campos = encabezados.map(x => x[1])
           let datos = products.map(product => {
-            let r = []
-            campos.forEach(campo => {
-              if (product.hasOwnProperty(campo)) r.push(product[campo])
-              else r.push("")
+            return campos.map(campo => {
+              if (product.hasOwnProperty(campo)) return product[campo]
+              return ""
             })
-
-            return r
           })
 
           generarDatos(datos)
