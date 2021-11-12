9aa273ad8e72087fa097dc27dca4bfa8b2a3a8f0
Rafael Ramirez <rafa.yael@gmail.com>
2021-11-11 20:41:17 -0600

    carrusel funcionado, responsivo

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ app.set("port", 5000)
 
 // Serve only the static files form the dist directory
 // app.use(express.static(__dirname + "/src"))
-var publicDir = path.join(__dirname, "src")
+app.use(express.static(__dirname + "/"))
+var publicDir = path.join(__dirname, "/")
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(publicDir, "index.html"))
 })
+
+
+
+
+// app.get("/*", (req, res) =>
+//   res.sendFile(path.join(__dirname + "/dist/index.html"))
+// )
 
 // Start the app by listening on the default Heroku port
 
