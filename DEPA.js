c0bff5d52c6aae9fd82ea192e32d6592e499ea77
Rafael Ramirez <rafa.yael@gmail.com>
2021-11-27 16:10:32 -0600

    fix: Tipo de letra Closes #3

--- DEPA.js
+++ DEPA.js
@@ -611,6 +611,13 @@ function scriptsEInicializacion() {
       href,
     }).appendTo("head")
   })
+  ;[
+    '<link rel="preconnect" href="https://fonts.googleapis.com">',
+    '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>',
+    '<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>',
+  ].forEach(link => {
+    $(link).appendTo("head")
+  })
 
   $.getScript(
     "//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js",
